92c86b5ca4c48c03e9972363549afbc44b592753
artemon1981 <141061991+artemon1981@users.noreply.github.com>
2023-12-26 15:43:43 +0300

    Update urls.py

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -6,9 +6,14 @@ from .views import UserViewSet, get_jwt_token, signup, user_profile
 v1_router = DefaultRouter()
 v1_router.register('users', UserViewSet)
 
+
+auth_path = [
+    path('auth/signup/', signup, name='signup),
+    path('auth/token/', get_jwt_token, name='get_jwt_token'),
+]
+
 urlpatterns = [
-    path('v1/auth/signup/', signup, name='signup'),
-    path('v1/auth/token/', get_jwt_token, name='get_jwt_token'),
     path('v1/users/me/', user_profile, name='user_profile'),
-    path('v1/', include(v1_router.urls)),
+    path('v1/', include(router_v1.urls)),
+    path('v1/', include(auth_path)),
 ]
