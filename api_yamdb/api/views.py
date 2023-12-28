21dfd48ace6ba2bc44448e856cd14335f486344b
Дмитрий Бойцев <dmboytsev@mail.ru>
2023-12-28 11:56:07 +0300

    ViewsForCommentAndReviews

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+from reviews.models import Review, Comment
+from .serializers import CommentSerializer, ReviewSerializer
+from .permissions import IsAdminAuthorModeratorOrReadOnly
+
+
+class BaseCommentReviewViewSet(viewsets.ModelViewSet):
+    serializer_class = None
+    model_class = None
+    permission_classes = (IsAdminAuthorModeratorOrReadOnly,)
+
+    def get_queryset(self):
+        instance = get_object_or_404(
+            self.model_class,
+            id=self.kwargs.get(f'{self.model_class.__name__.lower()}_id')
+        )
+        return instance.comments.all()
+
+    def perform_create(self, serializer):
+        instance = get_object_or_404(
+            self.model_class,
+            id=self.kwargs.get(f'{self.model_class.__name__.lower()}_id')
+        )
+        serializer.save(
+            author=self.request.user,
+            **{f'{self.model_class.__name__.lower()}': instance}
+        )
+
+
+class CommentViewSet(BaseCommentReviewViewSet):
+    serializer_class = CommentSerializer
+    model_class = Comment
+
+
+class ReviewViewSet(BaseCommentReviewViewSet):
+    serializer_class = ReviewSerializer
+    model_class = Review
